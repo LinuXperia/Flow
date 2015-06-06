53e0e9ded76661596bdda911be590344dc403f94
Chris Butcher <c.butcher@hotmail.com>
2015-06-06 14:17:43 -0700

    Updated the Component Interface to have the new methods.

--- core/interfaces/ComponentInterface.php
+++ core/interfaces/ComponentInterface.php
@@ -6,15 +6,22 @@
 	{
 		public function GetName();
 		public function SetName($ComponentName);
+
 		public function HasInPort($PortName);
-		public function HasOutPort($PortName);
+		public function GetInPorts();
 		public function SetInput($PortName, $Value);
 		public function GetInput($PortName);
+
+		public function HasOutPort($PortName);
 		public function GetOutput($PortName);
+		public function GetOutPorts();
+
 		public function IsStateful($IsStateful = false);
 		public function ResetComponent();
 		public function Initialize();
 		public function Execute();
+		public function IsExecuted($IsExecuted = null);
+		public function IsRunning($IsRunning = null);
 	}
 
 ?>
